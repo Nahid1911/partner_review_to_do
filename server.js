743c1624ff92991255b7302c4891028f3f18d763
Nahid1911 <nahid.raihan@gmail.com>
2023-05-16 15:06:03 +0600

    remove commented lines

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const app = express();
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
+
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
